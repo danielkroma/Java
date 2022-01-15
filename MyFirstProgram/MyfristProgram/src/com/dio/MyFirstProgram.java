74722e8a19b3792c74b00b55f0dcb8dd943b419b
Daniel <desenvolvedordaniel@gmail.com>
2022-01-15 14:31:21 -0300

    ajustando nomes

---
+++ MyFirstProgram/MyfristProgram/src/com/dio/MyFirstProgram.java
@@ -0,0 +1,15 @@
+package com.dio;
+import com.dio.order.*;
+
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        
+        Order order = new Order("123");
+
+        System.out.println(order);
+    }
+    
+
+
+}
---
+++ MyFirstProgram/MyfristProgram/src/com/dio/order/Order.java
@@ -0,0 +1,21 @@
+package com.dio.order;
+
+public class Order {
+
+    private final String code;
+
+    public Order(String code){
+
+        this.code=code;
+
+    } 
+
+    @Override
+    public String toString() {
+        
+        return "Order={"+
+               "code:'"+ code +"'" +
+               "}";
+    }
+
+}

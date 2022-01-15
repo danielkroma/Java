909f7dfd1b08bcc3262f32a264df8255597e4ce2
Daniel <desenvolvedordaniel@gmail.com>
2022-01-15 17:00:32 -0300

    condicionais

--- MyFirstProgram/src/com/dio/MyFirstProgram.java
+++ MyFirstProgram/src/com/dio/MyFirstProgram.java
@@ -4,10 +4,10 @@ import com.dio.order.*;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        
-        Order order = new Order("123");
 
-        System.out.println(order);
+        Order order = new Order("123",123);
+
+        System.out.println(order  );
     }
     
 
--- MyFirstProgram/src/com/dio/order/Order.java
+++ MyFirstProgram/src/com/dio/order/Order.java
@@ -3,19 +3,43 @@ package com.dio.order;
 public class Order {
 
     private final String code;
+    private int valor;
 
-    public Order(String code){
+
+    public Order(String code,int valor){
 
         this.code=code;
+        this.valor=valor;
 
     } 
 
+    public int calculateFeesIfElse(int valor){
+        if(valor>100){
+        return valor * 2;
+        }
+        else{
+            return valor;
+        }
+
+    }
+    public double calculateFeesCase(int valor){
+        switch(valor){
+            case 100:
+            return valor * 1.1;
+            case 200:
+            return valor * 2.2;
+            default:
+            return valor * 3.3;
+        }
+        
+
+    }
     @Override
     public String toString() {
         
         return "Order={"+
                "code:'"+ code +"'" +
-               "}";
+               " valor:'"+String.valueOf(calculateFeesCase(valor)) + "' }";
     }
 
 }

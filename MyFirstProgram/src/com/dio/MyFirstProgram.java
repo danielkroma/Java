c5595f5ef9cb1ae8eb6674e44ee8f0e43dd5d420
Daniel <desenvolvedordaniel@gmail.com>
2022-01-16 13:26:27 -0300

    inclusao de javadoc

--- MyFirstProgram/src/com/dio/MyFirstProgram.java
+++ MyFirstProgram/src/com/dio/MyFirstProgram.java
@@ -1,11 +1,22 @@
 package com.dio;
 import com.dio.order.*;
 
+/**
+ * @author Daniel Macedo Rodrigues
+ * @version 1.0.0
+ * @since 1.0.0 release
+ * 
+ */
 
 public class MyFirstProgram {
+
+    /**
+     * 
+     * @param args e uma forma do mundo externo comunicar-se com a aplicacao atraves de argumentos
+     */
     public static void main(String[] args) {
 
-        Order order = new Order("123",123);
+        Order order = new Order("123",-1);
 
         System.out.println(order  );
     }
--- MyFirstProgram/src/com/dio/order/Order.java
+++ MyFirstProgram/src/com/dio/order/Order.java
@@ -1,9 +1,25 @@
 package com.dio.order;
 
+import javax.management.RuntimeErrorException;
+
+/**
+ * @author Daniel Macedo Rodrigues
+ * @version 1.0.0
+ * @see int
+ * @see String
+ * @since 1.0.0 release
+ */
+
 public class Order {
 
     private final String code;
     private int valor;
+
+    /**
+     * 
+     * @param code representa o codigo do pedido
+     * @param valor representa o valor do pedido
+     */
 
 
     public Order(String code,int valor){
@@ -11,17 +27,34 @@ public class Order {
         this.code=code;
         this.valor=valor;
 
-    } 
+    }
+    
+    /**
+     * 
+     * @param valor valor recebido para aplicacaoo de um fator multiplicador representando a taxa cobrada
+     * @return o valor que sera calculado da taxa
+     * @throws RuntimeErrorException caso o valor seja menor do que zero
+     */
 
-    public int calculateFeesIfElse(int valor){
+    public int calculateFeesIfElse(int valor)throws RuntimeException{
         if(valor>100){
         return valor * 2;
         }
+        if(valor<0){
+            throw new RuntimeErrorException(null, "O valor nao pode ser negativo");
+            }
         else{
             return valor;
         }
 
     }
+
+    /**
+     * 
+     * @param valor valor recebido para aplicacao de um fator multiplicador representando a taxa cobrada
+     * @return  o valor que sera calculado da taxa se o metodo chamado for calculateFeesCase
+     */
+
     public double calculateFeesCase(int valor){
         switch(valor){
             case 100:
@@ -34,12 +67,15 @@ public class Order {
         
 
     }
+    /**
+     * @return descricao para saida do valor com codigo e taxa aplicada para o main 
+     */
     @Override
     public String toString() {
         
         return "Order={"+
                "code:'"+ code +"'" +
-               " valor:'"+String.valueOf(calculateFeesCase(valor)) + "' }";
+               " valor:'"+String.valueOf(calculateFeesIfElse(valor)) + "' }";
     }
 
 }

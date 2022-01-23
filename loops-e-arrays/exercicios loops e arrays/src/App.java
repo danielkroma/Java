ada0e84f962af7349fc9859a65181c8cafc727d0
Daniel <desenvolvedordaniel@gmail.com>
2022-01-23 10:43:28 -0300

    adicionado exercício 5

--- loops-e-arrays/exercicios loops e arrays/src/App.java
+++ loops-e-arrays/exercicios loops e arrays/src/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         //execucaoExercicio1.exercicioExecucao();
-        execucaoExercicio4.exercicioExecucao();
+        execucaoExercicio5.exercicioExecucao();
 
     }
 }
---
+++ loops-e-arrays/exercicios loops e arrays/src/execucaoExercicio5.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class execucaoExercicio5 {
+
+    public static void exercicioExecucao() {
+
+        System.out.println("Tabuada: Desenvolva um gerador de tabuada,"+
+        " capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10."+
+        " O usuário deve informar de qual numero ele deseja ver a tabuada."+
+        " A saída deve ser conforme o exemplo abaixo:\r\n"+
+
+        "Tabuada de 5:!\r\n"+
+        "5 X 1 = 5\r\n"+
+        "5 X 2 = 10\r\n"+
+        "...\r\n"+
+        "5 X 10 = 50\r\n");
+        System.out.print("informe um numero: \r\n");
+        Scanner entrada= new Scanner(System.in);
+        String saida = (String) entrada.nextLine();
+        System.out.println("Tabuada de "+ saida +" :\r\n");
+        for(int i= 1 ; i <= 10 ; i++){
+            System.out.println( saida + " X " + i + " = " +(Integer.valueOf(saida)* i));
+            
+ 
+
+        }
+
+    
+    }
+}
